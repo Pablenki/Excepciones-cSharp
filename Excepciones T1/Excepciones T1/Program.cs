e5945eeeb5265762bd4dc4802e33a3ed40b32271
Pablenki <105743191+Pablenki@users.noreply.github.com>
2022-05-19 23:09:16 -0700

    Programa 1
    excepciones try, catch, finally

---
+++ Excepciones T1/Excepciones T1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            try
+            {
+                Console.Write(" Ingrese el primer numero: ");
+                int Num1 = int.Parse(Console.ReadLine());
+                Console.Write(" Ingrese el segundo numero: ");
+                int Num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Num1 / Num2);
+                Console.ReadKey();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }     
+    }
+}
